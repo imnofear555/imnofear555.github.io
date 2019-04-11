cea0e960434fbc6994ded729ad8b18ecd773b1e6
Dmytryi Yashnyi <48893410+imnofear555@users.noreply.github.com>
2019-04-12 01:17:15 +0300

    Delete main.js

--- inter-com/js/main.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function(){
-    if ($(window).scrollTop() > 30) {
-        $('.navigat').addClass('scroll');
-    }
-    else {
-        $('.navigat').removeClass('scroll')
-    }
-});
