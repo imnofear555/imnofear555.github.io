c96054edac2d227f931219f3dd67600575dfec35
Dmytryi Yashnyi <48893410+imnofear555@users.noreply.github.com>
2019-04-12 16:01:41 +0300

    Add files via upload

---
+++ inter-com/php/zayavka-podkl.php
@@ -0,0 +1,41 @@
+<?php
+
+/* Задаем переменные */
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$tel = htmlspecialchars($_POST["tel"]);
+$addres = htmlspecialchars($_POST["addres"]);
+$message = htmlspecialchars($_POST["message"]);
+$bezspama = htmlspecialchars($_POST["bezspama"]);
+
+/* Ваш адрес и тема сообщения */
+$address = "dimaacer3@gmail.com";
+$sub = "Заявка на Подключение Inter-com";
+
+/* Формат письма */
+$mes = "Сообщение с сайта ХХХ.\n
+Имя отправителя: $name 
+Электронный адрес отправителя: $email
+Телефон отправителя: $tel
+Адрес отправителя: $addres
+Текст сообщения:
+$message";
+
+
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from = "Reply-To: $email \r\n";
+if (mail($address, $sub, $mes, $from)) {
+	header('Refresh: 5; URL=http://biznessystem.ru');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо отправлено, через 5 секунд вы вернетесь на страницу XXX</body>';}
+else {
+	header('Refresh: 5; URL=http://biznessystem.ru');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо не отправлено, через 5 секунд вы вернетесь на страницу YYY</body>';}
+}
+exit; /* Выход без сообщения, если поле bezspama заполнено спам ботами */
+?>
